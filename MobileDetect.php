d24a9610e88e6994d2d2f0110a80be0dae55a7ec
Nguyen Dang Hau <33993567+ngdanghau@users.noreply.github.com>
2018-08-14 16:42:10 +0700

    Create MobileDetect.php

---
+++ MobileDetect.php
@@ -0,0 +1,11 @@
+/**
+ * Detecting a mobile browser
+ * @return boolean 
+ */
+function MobileDetect() {
+    return preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipad|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|sagem|sharp|sie-|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT']) ? true : false;
+}
+
+/*
+echo MobileDetect() ? "true" : "false";
+*/
